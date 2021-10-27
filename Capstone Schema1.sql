5a9c341ddd8a2f8c5ef89f253658d991c4507d4f
KKTTAAII <krataias65@gmail.com>
2021-10-27 08:46:36 -0600

    first

---
+++ Capstone Schema1.sql
@@ -0,0 +1,53 @@
+﻿CREATE TABLE "hotel" (
+    "id" PRIMARY KEY,
+    "name" TEXT  NOT NULL,
+    "api_info" TEXT   NOT NULL
+);
+
+CREATE TABLE "restaurant" (
+    "id" PRIMARY KEY,
+    "name" TEXT   NOT NULL,
+    "api_info" TEXT   NOT NULL
+);
+
+CREATE TABLE "user" (
+    "id" PRIMARY KEY,
+    "first_name" TEXT   NOT NULL,
+    "last_name" TEXT   NOT NULL,
+    "phone_number" INT   NOT NULL,
+    "email" TEXT   NOT NULL,
+    "itinerary_count" INT  
+);
+
+CREATE TABLE "itinerary" (
+    "id" PRIMARY KEY,
+    "user_id" INT NOT NULL,
+    "start_date" DATE,
+    "end_date" DATE 
+);
+
+CREATE TABLE "itinerary_hotel" (
+    "itinerary_id" PRIMARY  KEY,
+    "hotel_id" PRIMARY KEY
+);
+
+CREATE TABLE "itinerary_rest" (
+    "itinerary_id" PRIMARY  KEY,
+    "rest_id" PRIMARY  KEY,
+);
+
+CREATE TABLE "favorties" (
+    "id" PRIMARY  KEY,
+    "user_id" INT  NOT NULL
+);
+
+CREATE TABLE "fav_hotel" (
+    "fav_id" PRIMARY  KEY,
+    "hotel_id" PRIMARY  KEY
+);
+
+CREATE TABLE "fav_rest" (
+    "fav_id" PRIMARY  KEY,
+    "rest_id" PRIMARY  KEY
+);
+
