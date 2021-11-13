7ec1f0588e5aa403315b6ef088a2f0fbfb542884
KKTTAAII <krataias65@gmail.com>
2021-11-13 00:21:47 -0700

    fix db url

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ errors = {
 }
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///user_itinerary'))
+    os.environ.get('DATABASE_URL', 'postgresql://user_itinerary'))
 
 app.config['API_KEY'] = os.environ.get('API_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
